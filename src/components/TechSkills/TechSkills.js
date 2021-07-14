f056c969e608cd5dc7354e5fa992a660a8b09c14
dr.sina121@gmail.com <khodabandehloo.sina@gmail.com>
2021-07-15 03:36:14 +0430

    add: TechSkills

--- src/components/TechSkills/TechSkills.js
+++ src/components/TechSkills/TechSkills.js
@@ -7,7 +7,25 @@ export const TechSkills = (props) => {
   return (
     <section className="tech-skills">
       <div className="section-title">Tech Skills & Qualifications</div>
-
+      <div className="subtitle">Programming Languages</div>
+      <div className="langs"><span>TypeScript</span><span>JavaScript</span></div>
+      <div className="subtitle">Tools</div>
+      <div className="techs">
+        <div>OS: Linux (Ubuntu)  &nbsp;  Windows</div>
+        <div>VCS: Git, TFVC</div>
+        <div>UI: HTML5, CSS3, Scss, Ant-Design, Material-UI, styled-components</div>
+        <div>SSR: Nextjs</div>
+        <div>Data Fetching: Ajax, Fetch, Axios, react-query, graphql, Postman</div>
+        <div>State Management: Context, MobX, Redux, Recoil</div>
+        <div>Testing: Jest, Enzyme</div>
+        <div>Type-Checking: TypeScript, PropTypes</div>
+        <div>Linting: EsLint</div>
+        <div>Build Systems: webpack, gulp, esbuild</div>
+        <div>Package Management: npm, yarn</div>
+        <div>Documentation: JsDoc, MarkDown</div>
+      </div>
+      <div className="subtitle">Concepts</div>
+      <div className="techs">ES6/ES7/ESNEXT,    OOP,    Regex,    Restful-API,    Cross-browser Compatibility,    Agile (Scrum),    GitFlow,   service-workers,  Clean-Coding</div>
     </section>
   );
 };
