fe141145ff82faa0b749775543c969395fbcf64c
dr.sina121@gmail.com <khodabandehloo.sina@gmail.com>
2021-07-14 16:53:46 +0430

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+
+function CV() {
+  return (
+    <div className="cv-container">
+      <div className="personal-info"></div>
+      <div className="about-me"></div>
+      <div className="work-experiences"></div>
+      <div className="tech-skills"></div>
+      <div className="other-skills"></div>
+      <div className="languages"></div>
+      <div className="hobbies"></div>
+    </div>
+  );
+}
+
+export default CV;
