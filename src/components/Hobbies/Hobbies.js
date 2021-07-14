c6388e9d69c0446c107f3f8146e7623debcaa7d9
dr.sina121@gmail.com <khodabandehloo.sina@gmail.com>
2021-07-15 04:19:58 +0430

    add: Hobbies

--- src/components/Hobbies/Hobbies.js
+++ src/components/Hobbies/Hobbies.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FaHeadphones } from 'react-icons/fa';
+import { GiChessPawn, GiYinYang } from 'react-icons/gi';
 
 import "./Hobbies.scss";
 
@@ -8,7 +10,9 @@ export const Hobbies = (props) => {
     <section className="hobbies">
       <div className="section-title">Hobbies</div>
       <div className="hobbies-container">
-        
+        <div className="hobby"><GiChessPawn /><div>Chess</div></div>
+        <div className="hobby"><GiYinYang /><div>Aikido</div></div>
+        <div className="hobby"><FaHeadphones /><div>Music</div></div>
       </div>
     </section>
   );
