6d3d83b9054253ee42a6f86fb846772436aca6f7
dr.sina121@gmail.com <khodabandehloo.sina@gmail.com>
2021-07-15 04:07:16 +0430

    fix: Languages styles

--- src/components/Hobbies/Hobbies.js
+++ src/components/Hobbies/Hobbies.js
@@ -4,7 +4,14 @@ import PropTypes from "prop-types";
 import "./Hobbies.scss";
 
 export const Hobbies = (props) => {
-  return <section className="hobbies">hobbies</section>;
+  return (
+    <section className="hobbies">
+      <div className="section-title">Hobbies</div>
+      <div className="hobbies-container">
+        
+      </div>
+    </section>
+  );
 };
 
 Hobbies.propTypes = {
