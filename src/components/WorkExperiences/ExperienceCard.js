11627d300b46a179546a1b1f8bd0a2df2c66df9d
dr.sina121@gmail.com <khodabandehloo.sina@gmail.com>
2021-07-16 03:21:57 +0430

    refactor: WorkExperiences to consume staticData.json

--- src/components/WorkExperiences/ExperienceCard.js
+++ src/components/WorkExperiences/ExperienceCard.js
@@ -3,23 +3,23 @@ import PropTypes from "prop-types";
 
 import "./WorkExperiences.scss";
 
-export const ExperienceCard = (props) => {
+export const ExperienceCard = ({ data }) => {
   return (
     <div className="experience-card-container">
       <div className="job-title">
           <span className="bullet"></span>
-          <span className="position">Front-End Developer & JS Consultant</span>
-          <span className="company">@Bedaan Inc</span>
+          <span className="position">{data.title}</span>
+          <span className="company">@{data.company}</span>
       </div>
-      <div className="subtitle">2020 JUN - Present (contract)</div>
-      <div className="description">I'm currently in charge of developing an advanced trading platform & portfolio optimization dashboard while offering best utils & packages & mentoring junior developers.</div>
-      <div className="description"><strong>Challenges</strong>: building a highly re-usable, customizable & scalable stockTables component for tens of different pages using the BLOC pattern & MVVM.</div>
-      <div className="description"><strong>Tech Stack</strong>: <span>React, MobX, handlebars, web-components, react-router, axios, i18next, Scss, apexcharts, eslint, jest, enzyme, rollup, webpack, markdown, react-beautiful-dnd, dayjs, typedjs</span></div>
-      <div className="description"><strong>Type-Checking</strong>: PropTypes &nbsp;<strong>Version Control</strong>: Git &nbsp;<strong>Team Collaboration</strong>: Taskulu</div>
+      <div className="subtitle">{data.timeline}</div>
+      <div className="description">{data.description}</div>
+      <div className="description"><strong>Challenges</strong>: {data.challenges}</div>
+      {!!data.techstack && <div className="description"><strong>Tech Stack</strong>: <span>{data.techstack}</span></div>}
+      {!!data.typechecking && !!data.versioncontrol  && !!data.collaboration && <div className="description"><strong>Type-Checking</strong>: {data.typechecking} &nbsp;<strong>Version Control</strong>: {data.versioncontrol} &nbsp;<strong>Team Collaboration</strong>: {data.collaboration}</div>}
     </div>
   );
 };
 
 ExperienceCard.propTypes = {
-  props: PropTypes.object,
+  data: PropTypes.object,
 };
--- src/components/WorkExperiences/WorkExperiences.js
+++ src/components/WorkExperiences/WorkExperiences.js
@@ -2,15 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import "./WorkExperiences.scss";
+import data from "./staticData.json";
 import { ExperienceCard } from "./ExperienceCard";
 
 export const WorkExperiences = (props) => {
   return (
     <section className="work-experiences">
       <div className="section-title">Work Experiences</div>
-      <ExperienceCard />
-      <ExperienceCard />
-      <ExperienceCard />
+      {data.map(desc => <ExperienceCard key={desc.company} data={desc} />)}
     </section>
   );
 };
